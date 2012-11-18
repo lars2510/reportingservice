466b2875e892d7b63dc2165e0ef73d46d247a0f2
lars schuettemeyer <lars.schuettemeyer@gmail.com>
2012-11-18 17:24:07 +0100

    model and view

--- src/main/java/com/jinengo/reporting/controller/HomeController.java
+++ src/main/java/com/jinengo/reporting/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.jinengo.reporting.service.HomeService;
 import com.jinengo.reporting.service.PersonDao;
@@ -22,11 +23,12 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String hello(Model model) {
-		
-		model.addAttribute("myName", personDao.selectUser("Lars").get(0).getEmail() );
-		
-		return "home";
+	public ModelAndView hello(Model model) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home");
+		//model.addAttribute("myName", personDao.selectUser("Lars").get(0).getEmail() );
+		modelAndView.addObject("myName", personDao.selectUser("Lars").get(0).getEmail());
+		return modelAndView;
 	}
 	
 }
